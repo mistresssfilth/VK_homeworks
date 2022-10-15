51316a7c209eca78001344a28fcae33c01539a89
mistressfilth <mistresssfilth@gmail.com>
2022-10-15 21:41:40 +0300

    hm1

--- controller/src/main/java/LibFactory.java
+++ controller/src/main/java/LibFactory.java
@@ -1,2 +1,14 @@
+import java.util.List;
+
 public class LibFactory {
+    private static Library library;
+    private List<Book> books;
+    private List<Author> authors;
+
+    public Library create(){
+        library = new Library(books, authors);
+        return library;
+    }
+
+
 }
--- controller/src/main/java/Library.java
+++ controller/src/main/java/Library.java
@@ -1,2 +1,28 @@
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
 public class Library {
+    private List<Book> books;
+    private List<Author> authors;
+
+    public void getBooksByAuthor(String name){
+        Author author = getAuthorByName(name);
+        if (author != null){
+            for(Book book : books){
+
+
+            }
+        }
+        else
+            System.out.println("Author not found");
+    }
+    private Author getAuthorByName(String name){
+        for(Author author : authors){
+            if (author.equals(name))
+                return author;
+        }
+        return null;
+    }
 }
